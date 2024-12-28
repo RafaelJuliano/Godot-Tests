292b1ba56f597b2096510a7f942dd910d78b1d36
RafaelJuliano <rafael.j.ferreira@hotmail.com>
2024-12-28 17:09:28 -0300

    animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,21 +6,37 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	handle_gravity(delta)
+	handle_jump()
+	handle_movement()
+
+func handle_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")) and is_on_floor():
+func handle_jump() -> void:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		animated_sprite.play("jump")
 		velocity.y = JUMP_VELOCITY
+		
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+func handle_movement() -> void:
+	var direction := Input.get_axis("move_left", "move_right")
+		
 	if direction:
-		animated_sprite.flip_h = direction < 1
-		velocity.x = direction * SPEED
+		set_is_running(direction)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		set_is_idle()
+		
 	move_and_slide()
+
+func set_is_idle() -> void:
+	if is_on_floor():
+		animated_sprite.play("idle")
+	velocity.x = move_toward(velocity.x, 0, SPEED)
+
+func set_is_running(direction: float) -> void:
+	if is_on_floor():
+		animated_sprite.flip_h = direction < 0
+		animated_sprite.play("run")
+	velocity.x = direction * SPEED
